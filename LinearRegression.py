fd3320841e9b1375e071e4afdd3107d15bf4c752
Kushagra Taneja <kushagrataneja2004@gmail.com>
2024-06-28 18:13:45 +0530

    Change the CSV path to relative

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #Importing the dataset
-dataset=pd.read_csv(r'D:\ML\Projects\SalaryPredictor\Salary_Data.csv')
+dataset=pd.read_csv('Salary_Data.csv')
 X=dataset.iloc[:, :-1].values
 Y=dataset.iloc[:,-1].values
 
